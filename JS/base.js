d8e78efd952cc27fdf025735ca139f7229410317
BastianBV <fitzz.barker@gmail.com>
2022-06-29 22:08:35 -0500

    recomendaciones

---
+++ JS/base.js
@@ -0,0 +1,25 @@
+console.log ('hola')
+//! Datos en Js 
+    //reglas o buenas practicas 
+        // No iniciar con digitos 
+            //let 9variable = 9; ---> ERROR 
+        // No acepta palabra reservadas 
+            // Let else =  'else'----> ERROR  
+        // Consideracion: Sensible a las Mayus y Minis 
+        // Buena Practica: camelCase 99% - PascalCase --> Clases - "CONSTANTE" En mayuscula 
+        // Buena Practica: Naming descriptivo --> let estudiantesKodemia = 'koders'
+
+//! Variables y Constatnte
+      // Variable  =  Mutabilidad = VAR - LET
+      // Constante = No Cambia  = CONST 
+let edadkoder = 25
+var alturakoder = 185
+            
+
+// palabraReservada -> identificador -> operdadorAsignacion -> Valor
+console.log(edadkoder);
+console.log(alturakoder);     
+
+
+
+            // RECOMENDACION/ Buena practica siempre usar LET 
